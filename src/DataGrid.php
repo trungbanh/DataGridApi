b45fd2015fa19b0a5206ad098416cf41ab45c602
trungbanh <trungbanh97@gmail.com>
2022-05-19 06:02:38 +0700

    update filter and sort

--- src/DataGrid.php
+++ src/DataGrid.php
@@ -52,6 +52,9 @@ abstract class DataGrid
     protected $itemsPerPage = 10;
 
 
+    protected $enabledFilterMap = false;
+
+
     protected $operators = [
         'eq'       => '=',
         'lt'       => '<',
@@ -70,6 +73,19 @@ abstract class DataGrid
         'regex'    => 'regexp',
         'notregex' => 'not regexp',
     ];
+
+    /**
+     *
+     * @param string $alias
+     * @param string $column
+     * @return void
+     */
+    public function addFilter($alias, $column)
+    {
+        $this->filterMap[$alias] = $column;
+        $this->enabledFilterMap = true;
+    }
+
 
     /**
      * Complete column details.
@@ -236,6 +252,143 @@ abstract class DataGrid
         foreach ($this->completedColumnsDetails as $column) {
             if ($column['index'] == $columnAlias) {
                 return [$column['type'], $column['index']];
+            }
+        }
+    }
+
+
+    public function exceptionCheckInColumns($columnName)
+    {
+        foreach ($this->completedColumnsDetails as $column) {
+            if ($column['index'] === $columnName && !$column['filterable']) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Resolve query.
+     *
+     * @param  object  $query
+     * @param  string  $columnName
+     * @param  string  $condition
+     * @param  string  $filterValue
+     * @param  null|boolean  $nullCheck
+     * @return void
+     */
+    private function resolveQuery($query, $columnName, $condition, $filterValue, $clause = 'where')
+    {
+        $query->$clause(
+            $columnName,
+            $this->operators[$condition],
+            $filterValue
+        );
+    }
+
+
+    /**
+     * Main resolve method.
+     *
+     * @param  \Illuminate\Support\Collection  $collection
+     * @param  string  $columnName
+     * @param  string  $condition
+     * @param  string  $filterValue
+     * @param  string  $clause
+     * @param  string  $method
+     * @return void
+     */
+    private function resolve($collection, $columnName, $condition, $filterValue, $clause = 'where', $method = 'resolveQuery')
+    {
+        if ($this->enabledFilterMap && isset($this->filterMap[$columnName])) {
+            $this->$method($collection, $this->filterMap[$columnName], $condition, $filterValue, $clause);
+        } else if ($this->enabledFilterMap && !isset($this->filterMap[$columnName])) {
+            $this->$method($collection, $columnName, $condition, $filterValue, $clause);
+        } else {
+            $this->$method($collection, $columnName, $condition, $filterValue, $clause);
+        }
+    }
+
+
+    /**
+     * Sort collection.
+     *
+     * @param  \Illuminate\Support\Collection  $collection
+     * @param  array  $info
+     * @return void
+     */
+    private function sortCollection($collection, $info)
+    {
+        $availableOptions = ['asc', 'desc'];
+        $selectedSortOption = strtolower(array_values($info)[0]);
+        $countKeys = count(array_keys($info));
+        if ($countKeys > 1) {
+            throw new \Exception(__('not support for multi-sort'));
+        }
+
+        $columnName = $this->findColumnType(array_keys($info)[0]);
+        $collection->orderBy(
+            $columnName[1],
+            in_array($selectedSortOption, $availableOptions) ? $selectedSortOption : 'asc'
+        );
+    }
+
+
+    /**
+     * Search collection.
+     *
+     * @param  \Illuminate\Support\Collection  $collection
+     * @param  array  $info
+     * @return void
+     */
+    public function searchCollection($collection, $info)
+    {
+        $countKeys = count(array_keys($info));
+
+        if ($countKeys > 1) {
+            throw new \Exception("not support for multi-search", 1);
+        }
+
+        if ($countKeys == 1) {
+            $collection->where(function ($collection) use ($info) {
+                foreach ($this->completedColumnsDetails as $column) {
+                    if ($column['searchable'] == true) {
+                        $this->resolve($collection, $column['index'], 'like', '%' . $info['all'] . '%', 'orWhere');
+                    }
+                }
+            });
+        }
+    }
+
+
+    /**
+     * Filter collection.
+     *
+     * @param  \Illuminate\Support\Collection  $collection
+     * @param  array  $info
+     * @param  string  $columnType
+     * @param  string  $columnName
+     * @return void
+     */
+    private function filterCollection($collection, $info, $columnType, $columnName)
+    {
+        if (array_keys($info)[0] === 'like' || array_keys($info)[0] === 'nlike') {
+            foreach ($info as $condition => $filterValue) {
+                $this->resolve($collection, $columnName, $condition, '%' . $filterValue . '%');
+            }
+        } else {
+            foreach ($info as $condition => $filterValue) {
+
+                $condition = ($condition === 'undefined') ? '=' : $condition;
+
+                match ($columnType) {
+                    'boolean'  => $this->resolve($collection, $columnName, $condition, $filterValue, 'where', 'resolveBooleanQuery'),
+                    'checkbox' => $this->resolve($collection, $columnName, $condition, $filterValue, 'whereIn', 'resolveCheckboxQuery'),
+                    'price'    => $this->resolve($collection, $columnName, $condition, $filterValue, 'having'),
+                    'datetime' => $this->resolve($collection, $columnName, $condition, $filterValue, 'whereDate'),
+                    default    => $this->resolve($collection, $columnName, $condition, $filterValue)
+                };
             }
         }
     }
